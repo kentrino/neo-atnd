feeadad1cba8668db2b73b6706b4a52b1b0049e7
kentrino <kentrino@h-kento.jp>
2016-05-19 22:03:31 +0900

    Fix calback path

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  configure do |config|
+    config.full_host = 'https://atnd.h-kento.jp'
+  end
+
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
